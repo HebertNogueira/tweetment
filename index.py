0646513a4599d35ad7ec7619c840a08e659b4f4b
Hebert Fracis Amaro Nogueira <nogueira.hebert@gmail.com>
2019-09-16 10:14:50 -0300

    Create API

--- index.py
+++ index.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, redirect, url_for
 from connect_mongo import ConnectMongo as cm
+from bson.objectid import ObjectId
+from bson.json_util import dumps
 import os
 
 app = Flask(__name__)
+
+@app.route('/trending/<string:ttd>', methods=['GET'])
+def catchTrendigParams(ttd):
+	client = cm.connectMongo()
+	db = client.trendingsDisplay
+	result = db.trendingTopicsDisplay.find({'_id': ObjectId(ttd)})
+	return  dumps(result)
 
 @app.route('/')
 def index():
 	result = catch_tt()
 	return render_template('index.html', result=result)
-
 
 def catch_tt():
 	client = cm.connectMongo()
